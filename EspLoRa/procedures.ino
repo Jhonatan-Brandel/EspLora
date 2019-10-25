d9dfc69b62eac154a0526ee4c82f50655403467d
Jhonatan Brandel <jhonatanbrandel@gmail.com>
2019-10-25 17:01:12 -0300

    EspLoRa mais organizado, e novos estilos CSS.

---
+++ EspLoRa/procedures.ino
@@ -0,0 +1,39 @@
+void startListeners()
+{ SPIFFS.begin();
+  delay(10);
+  File file = SPIFFS.open("/apconf.txt","r");
+  String txt=String(file2str(file));
+  file.close();
+  if(txt.equals("")){
+   WiFi.softAP(ssid, password);}
+  else
+  {
+    String ssid2=get_pseudo_dict_data(txt,"#SSID");
+    String password2=get_pseudo_dict_data(txt,"#PWD");
+    WiFi.softAP(ssid2, password2);
+  }
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  autoconnect();
+  WiFi.hostname("esp8266");
+  
+  server.on("/", handleRoot);
+  server.on("/status", handleRoot);
+  server.on("/options",handle_options);
+  server.on("/softapconfig",handle_ap_config);
+  server.on("/actionapconfig", action_ap_config);
+  server.on("/stconfig",handle_st_config);
+  server.on("/actionstconfig", action_st_config);
+  server.on("/connect",handle_connect);
+  server.on("/actionconnect",handle_action_connect);
+  server.on("/actionautoconnect",handle_action_autocon);
+  server.on("/autoconxml", autoconXMLcontent);  
+  server.on("/goback",goback);
+  server.on("/conf.txt",handle_conf_file);
+  server.on("/connectxml", XMLcontent);
+  server.begin();
+  Serial.println("HTTP server started");
+
+}
+
